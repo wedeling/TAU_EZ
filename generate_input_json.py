358c1119654b42acb325e53e0bc945f311a052a9
wedeling <wouteredeling@gmail.com>
2019-02-17 21:45:41 +0100

    leave1out of zeus

--- generate_input_json.py
+++ generate_input_json.py
@@ -4,13 +4,14 @@ import os
 
 HOME = os.path.abspath(os.path.dirname(__file__))
 
-fname = 'tau_src_lag1_lag_efold'
+fname = 'zeu_1lag'
 target = ['dE', 'dZ']
 #covariates = [['r_tau_E*sprime_n_LF', 'r_tau_Z*zprime_n_LF'], ['r_tau_Z*zprime_n_LF', 'r_tau_E*sprime_n_LF']]
 #covariates = [['z_n_LF', 'e_n_LF', 'u_n_LF', 's_n_LF'], ['z_n_LF', 'e_n_LF', 'u_n_LF', 's_n_LF']]
-covariates = [['r_tau_E*sprime_n_LF', 'r_tau_E*sprime_n_LF'], ['r_tau_Z*zprime_n_LF', 'r_tau_Z*zprime_n_LF']]
+covariates = [['z_n_LF', 'e_n_LF', 'u_n_LF'], ['z_n_LF', 'e_n_LF', 'u_n_LF']]
+#covariates = [['r_tau_E*sprime_n_LF', 'r_tau_E*sprime_n_LF'], ['r_tau_Z*zprime_n_LF', 'r_tau_Z*zprime_n_LF']]
 
-lag = [[1, 2], [1, 2]]
+lag = [[1, 1, 1], [1, 1, 1]]
 
 N_surr = len(target)
 
